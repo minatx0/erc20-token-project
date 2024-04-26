70fab72613216470e2690b4fac0366af9b7ea3c2
minatx0 <160779971+minatx0@users.noreply.github.com>
2024-04-26 10:23:33 +0200

    added function

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,31 +1,58 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract MyToken {
     string public name = "MyToken";
     string public symbol = "MTK";
     uint8 public decimals = 18;
-    uint256 public totalSupply;
+    uint256 private _totalSupply;
+
+    address public owner;
+    bool public paused = false;
 
     mapping(address => uint256) private balances;
     mapping(address => mapping(address => uint256)) private allowed;
+    mapping(address => bool) public minters;
 
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Paused();
+    event Unpaused();
+    event MinterAdded(address indexed minter);
+    event MinterRemoved(address indexed minter);
+    event Burn(address indexed burner, uint256 value);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused, "Contract is paused");
+        _;
+    }
+
+    modifier canMint() {
+        require(minters[msg.sender], "Cannot mint");
+        _;
+    }
 
     constructor(uint256 _initialSupply) {
-        totalSupply = _initialSupply * 10**uint256(decimals);
-        balances[msg.sender] = totalSupply; 
+        owner = msg.sender;
+        _totalSupply = _initialSupply * 10**uint256(decimals);
+        balances[msg.sender] = _totalSupply;
+        minters[msg.sender] = true; // Initially, only the creator can mint
     }
 
     function totalSupply() external view returns (uint256) {
-        return totalSupply;
+        return _totalSupply;
     }
 
     function balanceOf(address _owner) external view returns (uint256 balance) {
         return balances[_owner];
     }
 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
+    function transfer(address _to, uint256 _value) external whenNotPaused returns (bool success) {
         require(balances[msg.sender] >= _value, "Insufficient balance");
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -33,7 +60,7 @@ contract MyToken {
         return true;
     }
 
-    function transferFrom(address _from, address _to, uint256 _value) external returns (bool success) {
+    function transferFrom(address _from, address _to, uint256 _value) external whenNotPaused returns (bool success) {
         require(_value <= balances[_from], "Insufficient balance");
         require(_value <= allowed[_from][msg.sender], "Not authorized");
         
@@ -55,11 +82,39 @@ contract MyToken {
         return allowed[_owner][_spender];
     }
 
-    function mint(address _to, uint256 _amount) external returns (bool) {
-        require(msg.sender == address(this), "Only contract can mint");
-        totalSupply += _amount;
+    function mint(address _to, uint256 _amount) external canMint returns (bool) {
+        _totalSupply += _amount;
         balances[_to] += _amount;
         emit Transfer(address(0), _to, _amount);
         return true;
+    }
+
+    function burn(uint256 _amount) external returns (bool) {
+        require(balances[msg.sender] >= _amount, "Insufficient balance");
+        _totalSupply -= _amount;
+        balances[msg.sender] -= _amount;
+        emit Burn(msg.sender, _amount);
+        emit Transfer(msg.sender, address(0), _amount);
+        return true;
+    }
+
+    function pause() external onlyOwner {
+        paused = true;
+        emit Paused();
+    }
+
+    function unpause() external onlyOwner {
+        paused = false;
+        emit Unpaused();
+    }
+
+    function addMinter(address _minter) external onlyOwner {
+        minters[_minter] = true;
+        emit MinterAdded(_minter);
+    }
+
+    function removeMinter(address _minter) external onlyOwner {
+        minters[_minter] = false;
+        emit MinterRemoved(_minter);
     }
 }
